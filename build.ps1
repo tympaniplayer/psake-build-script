3b394b3c11ee4aa3a1f44bddc4492356de551c0f
Nathan Palmer <Nathan Palmer>
2016-01-24 16:11:44 -0500

    Initial commit with initial build script

---
+++ build.ps1
@@ -0,0 +1,18 @@
+﻿Clear-Host
+
+Remove-Module [p]sake
+
+$psakeModule = (Get-ChildItem (".\src\BuildToolKit\Packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -Last 1
+
+Import-Module $psakeModule
+
+Invoke-psake -buildFile  .\src\BuildToolKit\Build\default.ps1 `
+			 -taskList Test `
+             -framework 4.5.2 `
+			 -properties @{ "buildConfiguration" = "Release"
+                            "buildPlatform" = "Any CPU"} `
+			 -parameters @{ "solutionFile" = "..\BuildToolKit.sln" } 
+
+Write-Host "Build exit code:" $LASTEXITCODE
+
+exit $LASTEXITCODE
